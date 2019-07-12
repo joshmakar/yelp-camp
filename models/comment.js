b01b521f750884dab155818ee3cc7043d71f793c
joshmakar <joshmakar@gmail.com>
2019-07-12 17:46:30 -0400

    Changed the commentSchema author from a String to an object and added a username key. This was done in order to omit having to have an author field on the comment form and to save time by reducing database calls

--- models/comment.js
+++ models/comment.js
@@ -11,6 +11,13 @@ const mongoose    = require('mongoose');
 
 const commentSchema = new mongoose.Schema({
   text: String,
-  author: String
+  author: {
+    id: {
+      type: mongoose.Schema.Types.ObjectId,
+      re: "User"
+    },
+    username: String
+  }
 });
+
 module.exports = mongoose.model('Comment', commentSchema);
