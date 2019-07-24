f61f520050aab1bf27b9b07e8a20748e995cd00b
joshmakar <joshmakar@gmail.com>
2019-07-24 18:15:14 -0400

    Added some additional information to users

--- models/user.js
+++ models/user.js
@@ -2,8 +2,12 @@ const mongoose              = require('mongoose'),
       passportLocalMongoose = require('passport-local-mongoose');
 
 const UserSchema = new mongoose.Schema({
+  email: String,
   username: String,
   password: String,
+  firstName: String,
+  lastName: String,
+  avatar: String,
   isAdmin: {
     type: Boolean,
     default: false
