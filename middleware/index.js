fc056912a08d6c6f385234b4a8892b69cc9aef2d
joshmakar <joshmakar@gmail.com>
2019-09-24 11:40:20 -0400

    Convert middleware obj to class

--- middleware/index.js
+++ middleware/index.js
@@ -11,57 +11,58 @@ const Campground = require('../models/campground'),
 // Middleware
 //////////////////////////////////////////////////
 
-// Create object to hold middleware functions
-const middlewareObj = {};
-
-// User Authentication
-middlewareObj.isLoggedIn = (req, res, next) => {
-  if (!req.isAuthenticated()) {
-    req.flash('error', 'You must <a href="/login">log in</a> to perform this action.');
-    return res.redirect('back');
-  }
-  next();
-}
-
-// Campground Authorization
-middlewareObj.isAuthorizedCampground = (req, res, next) => {
-  if (!req.isAuthenticated()) {
-    req.flash('error', 'You must <a href="/login">log in</a> to perform this action.');
-    return res.redirect('back');
-  }
-  Campground.findById(req.params.id, (err, foundCampground) => {
-    if (err) {
-      console.log(err);
-      req.flash('error', 'Campground not found.');
-      return res.redirect('back');
-    }
-    if (!req.user.isAdmin && !foundCampground.author.id.equals(req.user._id)) {
-      req.flash('error', 'You do not have permission to perform this action.');
+class Middleware {
+  
+  // User Authentication
+  static isLoggedIn(req, res, next) {
+    if (!req.isAuthenticated()) {
+      req.flash('error', 'You must <a href="/login">log in</a> to perform this action.');
       return res.redirect('back');
     }
     next();
-  });
+  }
+  
+  // Campground Authorization
+  static isAuthorizedCampground(req, res, next) {
+    if (!req.isAuthenticated()) {
+      req.flash('error', 'You must <a href="/login">log in</a> to perform this action.');
+      return res.redirect('back');
+    }
+    Campground.findById(req.params.id, (err, foundCampground) => {
+      if (err) {
+        console.log(err);
+        req.flash('error', 'Campground not found.');
+        return res.redirect('back');
+      }
+      if (!req.user.isAdmin && !foundCampground.author.id.equals(req.user._id)) {
+        req.flash('error', 'You do not have permission to perform this action.');
+        return res.redirect('back');
+      }
+      next();
+    });
+  }
+  
+  // Comment Authorization
+  static isAuthorizedComment (req, res, next) {
+    if (!req.isAuthenticated()) {
+      req.flash('error', 'You must <a href="/login">log in</a> to perform this action.');
+      return res.redirect('back');
+    }
+    Comment.findById(req.params.comment_id, (err, foundComment) => {
+      if (err) {
+        console.log(err);
+        req.flash('error', 'Comment not found.');
+        return res.redirect('back');
+      }
+      if (!req.user.isAdmin && !foundComment.author.id.equals(req.user._id)) {
+        req.flash('error', 'You do not have permission to perform this action.');
+        return res.redirect('back');
+      }
+      next();
+    });
+  }
 }
 
-// Comment Authorization
-middlewareObj.isAuthorizedComment = (req, res, next) => {
-  if (!req.isAuthenticated()) {
-    req.flash('error', 'You must <a href="/login">log in</a> to perform this action.');
-    return res.redirect('back');
-  }
-  Comment.findById(req.params.comment_id, (err, foundComment) => {
-    if (err) {
-      console.log(err);
-      req.flash('error', 'Comment not found.');
-      return res.redirect('back');
-    }
-    if (!req.user.isAdmin && !foundComment.author.id.equals(req.user._id)) {
-      req.flash('error', 'You do not have permission to perform this action.');
-      return res.redirect('back');
-    }
-    next();
-  });
-}
 
 // Export middleware
-module.exports = middlewareObj;
+module.exports = Middleware;
